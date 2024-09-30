6d232ba9a16daad52ffa9910c281634649a45a45
Tushar Ojha <tusharojha2001@gmail.com>
2024-09-30 12:14:20 +0530

    improved attestation structure

--- pallets/algorithms/src/lib.rs
+++ pallets/algorithms/src/lib.rs
@@ -70,69 +70,6 @@ pub mod pallet {
 
     #[pallet::call]
     impl<T: Config> Pallet<T> {
-        #[pallet::call_index(0)]
-        #[pallet::weight(100_000)]
-        pub fn run_algo(origin: OriginFor<T>, a: i32, b: i32, wasm: Vec<u8>) -> DispatchResult {
-            let who = ensure_signed(origin)?;
-
-            let engine = wasmi::Engine::default();
-
-            let module =
-                wasmi::Module::new(&engine, wasm.as_slice()).map_err(|_| Error::<T>::AlgoError1)?;
-
-            type HostState = u32;
-            let mut store = wasmi::Store::new(&engine, 42);
-            let host_print = wasmi::Func::wrap(
-                &mut store,
-                |caller: wasmi::Caller<'_, HostState>, param: i32| {
-                    log::debug!(target: "algo", "Message:{:?}", param);
-                },
-            );
-            let memory = wasmi::Memory::new(
-                &mut store,
-                wasmi::MemoryType::new(8, None).map_err(|_| Error::<T>::AlgoError2)?,
-            )
-                .map_err(|_| Error::<T>::AlgoError2)?;
-
-            memory.write(&mut store, 0, &a.to_ne_bytes()).map_err(|e| {
-                log::error!(target: "algo", "Algo1 {:?}", e);
-                Error::<T>::AlgoError1
-            })?;
-            memory.write(&mut store, 4, &b.to_ne_bytes()).map_err(|e| {
-                log::error!(target: "algo", "Algo1 {:?}", e);
-                Error::<T>::AlgoError1
-            })?;
-            // memory.write(&mut store, 0, 5);
-
-            let mut linker = <wasmi::Linker<HostState>>::new(&engine);
-            linker.define("host", "print", host_print).map_err(|_| Error::<T>::AlgoError2)?;
-            linker.define("env", "memory", memory).map_err(|_| Error::<T>::AlgoError2)?;
-
-            let instance = linker
-                .instantiate(&mut store, &module)
-                .map_err(|e| {
-                    log::error!(target: "algo", "Algo3 {:?}", e);
-                    Error::<T>::AlgoError3
-                })?
-                .start(&mut store)
-                .map_err(|_| Error::<T>::AlgoError4)?;
-
-            let hello = instance
-                .get_typed_func::<(), i64>(&store, "calc")
-                .map_err(|_| Error::<T>::AlgoError5)?;
-
-            // And finally we can call the wasm!
-            let a = hello.call(&mut store, ()).map_err(|e| {
-                log::error!(target: "algo", "Algo6 {:?}", e);
-                Error::<T>::AlgoError6
-            })?;
-            Self::deposit_event(Event::AlgoResult {
-                result: a,
-            });
-
-            Ok(())
-        }
-
         #[pallet::call_index(1)]
         #[pallet::weight(100_000)]
         pub fn save_algo(origin: OriginFor<T>, schema_hashes: Vec<T::Hash>, code: Vec<u8>) -> DispatchResult {
@@ -154,10 +91,9 @@ pub mod pallet {
             Ok(())
         }
 
-
         #[pallet::call_index(2)]
         #[pallet::weight(100_000)]
-        pub fn run_algo_for(origin: OriginFor<T>, account_id: Vec<u8>, algorithm_id: u64) -> DispatchResult {
+        pub fn run_algo_for(origin: OriginFor<T>, issuer_hash: T::Hash, account_id: Vec<u8>, algorithm_id: u64) -> DispatchResult {
             let who = ensure_signed(origin)?;
 
             let acquirer_address = credentials::Pallet::<T>::parse_acquirer_address(account_id)?;
@@ -166,9 +102,10 @@ pub mod pallet {
 
             let mut attestations: Vec<pallet_credentials::CredAttestation> = Vec::<>::with_capacity(algorithm.schema_hashes.len());
 
-            for schema_hash in algorithm.schema_hashes {
-                let attestation = Attestations::<T>::get(acquirer_address.clone(), schema_hash).ok_or(crate::pallet::Error::<T>::AttestationNotFound)?;
-                attestations.push(attestation);
+            for schema_hash in &algorithm.schema_hashes {
+              let attestation = Attestations::<T>::get((acquirer_address.clone(), issuer_hash, *schema_hash))
+                  .ok_or(Error::<T>::AttestationNotFound)?;
+              attestations.push(attestation);
             }
 
 
--- pallets/credentials/src/lib.rs
+++ pallets/credentials/src/lib.rs
@@ -92,12 +92,13 @@ pub mod pallet {
     pub type Schemas<T: Config> = StorageMap<_, Blake2_128Concat, T::Hash, CredSchema, OptionQuery>;
 
     #[pallet::storage]
-    pub type Attestations<T: Config> = StorageDoubleMap<
+    pub type Attestations<T: Config> = StorageNMap<
         _,
-        Blake2_128Concat,
-        AcquirerAddress,
-        Twox64Concat,
-        T::Hash,
+        (
+            NMapKey<Blake2_128Concat, AcquirerAddress>, // Reciever account.
+            NMapKey<Twox64Concat, T::Hash>,  // Issuer hash.
+            NMapKey<Twox64Concat, T::Hash>,      // Schema hash.
+        ),
         CredAttestation,
         OptionQuery,
     >;
@@ -110,6 +111,7 @@ pub mod pallet {
             schema: CredSchema,
         },
         AttestationCreated {
+            issuer_hash: T::Hash,
             account_id: AcquirerAddress,
             schema_hash: T::Hash,
             attestation: CredAttestation,
@@ -192,9 +194,13 @@ pub mod pallet {
 
             log::debug!(target: "algo", "Creds:{:?}", attestation);
 
-            Attestations::<T>::insert(acquirer_address.clone(), schema_hash, attestation.clone());
+            Attestations::<T>::insert(
+              (acquirer_address.clone(), issuer_hash, schema_hash),
+              attestation.clone(),
+            );
 
             Self::deposit_event(Event::AttestationCreated {
+                issuer_hash,
                 account_id: acquirer_address,
                 schema_hash,
                 attestation,
@@ -231,38 +237,13 @@ pub mod pallet {
             Some(formatted)
         }
 
-        pub fn is_valid_solana_address(address: &H256) -> bool {
-            let address_bytes: &[u8] = address.as_ref();
-            
-            if address_bytes.len() != 32 {
-                return false;
-            }
-
-            let address_array: [u8; 32] = match address_bytes.try_into() {
-                Ok(arr) => arr,
-                Err(_) => return false,
-            };
-            
-            log::info!("sol address, check passed a little bits by");
-            
-            if VerifyingKey::from_bytes(&address_array).is_err() {
-                return false;
-            }
-
-            if address.as_ref().iter().all(|&x| x == 0) {
-                return false;
-            }
-
-            let default_pubkey = H256::from_slice(&[1; 32]);
-            if address == &default_pubkey {
-                return false;
-            }
-
-            true
-        }
-
-        fn to_hex_string(bytes: &[u8]) -> String {
-            bytes.iter().map(|b| format!("{:02x}", b)).collect()
+        pub fn is_valid_solana_address(address: Vec<u8>) -> bool {
+          match address.try_into() {
+              Ok(address_array) => {
+                  VerifyingKey::from_bytes(&address_array).is_ok()
+              }
+              Err(_) => false,
+          }
         }
 
         pub fn check_valid_substrate_address(address: &[u8]) -> Option<AccountId32> {
@@ -271,46 +252,54 @@ pub mod pallet {
             AccountId32::from_ss58check(&account_id.to_ss58check()).ok().map(|_| account_id)
         }
 
-        pub fn is_solana_address(input: Vec<u8>) -> Option<String> {
-            let base58_str = core::str::from_utf8(&input).ok()?;
-
-            if input.len() == 32 {
-                return Some(String::from(base58_str));
-            }
-
-            let mut decoded_output = [0u8; 64];
-            match bs58::decode(base58_str).onto(&mut decoded_output) {
-                Ok(32) => Some(String::from(base58_str)),
-                Ok(decoded_length) => {
-                    log::error!("Decoded length is not 32 bytes: {}", decoded_length);
-                    None
-                }
-                Err(e) => {
-                    log::error!("Failed to decode Base58: {:?}", e);
-                    None
-                }
-            }
-        }
+        pub fn check_solana_address(input: Vec<u8>) -> Option<String> {
+          let base58_str = core::str::from_utf8(&input).ok()?;
+      
+          let mut decoded_output = [0u8; 32];
+          match bs58::decode(base58_str).onto(&mut decoded_output) {
+              Ok(32) => {
+                  if Self::is_valid_solana_address(decoded_output.to_vec()) {
+                      Some(String::from(base58_str))
+                  } else {
+                      None
+                  }
+              },
+              Ok(decoded_length) => {
+                  log::error!("Decoded length is not 32 bytes: {}", decoded_length);
+                  None
+              }
+              Err(e) => {
+                  log::error!("Failed to decode Base58: {:?}", e);
+                  None
+              }
+          }
+      }
 
         pub fn parse_acquirer_address(address: Vec<u8>) -> Result<AcquirerAddress, DispatchError> {
-            if let Some(solana_address) = Self::is_solana_address(address.clone()) {
-                return Ok(AcquirerAddress::Solana(solana_address));
-            }
+          if let Some(solana_address) = Self::check_solana_address(address.clone()) {
+              return Ok(AcquirerAddress::Solana(solana_address));
+          }
+      
+          match address.len() {
+              20 => {
+                  let mut array = [0u8; 20];
+                  array.copy_from_slice(&address);
+                  let wallet_address = H160::from(array);
+                  Ok(AcquirerAddress::Ethereum(wallet_address))
+              },
+              _ => {
+                  // Try to decode as a Substrate address
+                let address_str = core::str::from_utf8(&address)
+                .map_err(|_| Error::<T>::InvalidAddress)?;
 
-            match address.len() {
-                20 => {
-                    let mut array = [0u8; 20];
-                    array.copy_from_slice(&address);
-                    let wallet_address = H160::from(array);
-                    Ok(AcquirerAddress::Ethereum(wallet_address))
-                },
-                _ if address.len() == T::AccountId::max_encoded_len() => {
-                    Self::check_valid_substrate_address(&address)
-                        .map(AcquirerAddress::Substrate)
-                        .ok_or_else(|| DispatchError::Other("Invalid Substrate address format"))
-                },
-                _ => Err(Error::<T>::InvalidFormat.into()),
-            }
+                match AccountId32::from_ss58check(address_str) {
+                  Ok(account_id32) => {
+                      Ok(AcquirerAddress::Substrate(account_id32))
+                  },
+                  Err(_) => Err(Error::<T>::InvalidAddress.into()),
+                }
+              },
+          }
         }
     }
 }
